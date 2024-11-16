8f061e35d461eb6a624e4a693a03d2d2b3951f20
cmo <fourty2@126.com>
2024-11-16 15:16:29 +0800

    update stat method

--- model_cmo/my_stat.py
+++ model_cmo/my_stat.py
@@ -36,11 +36,7 @@ class StatisticalAnalysis:
             "tes": {
                 # TODO: refine the range
                 # "range": np.linspace(0.9, 1.1, 15),
-<<<<<<< HEAD
                 "range": np.linspace(0.9, 1.1, 3),
-=======
-                "range": np.linspace(0.9, 1.1, 7),
->>>>>>> parent of 9cd1d09... first submission
                 "mean": 1.0,
                 "std": 0.01,
             },
@@ -52,11 +48,7 @@ class StatisticalAnalysis:
             "jes": {
                 # TODO: refine the range
                 # "range": np.linspace(0.9, 1.1, 15),
-<<<<<<< HEAD
                 "range": np.linspace(0.9, 1.1, 3),
-=======
-                "range": np.linspace(0.9, 1.1, 10),
->>>>>>> parent of 9cd1d09... first submission
                 "mean": 1.0,
                 "std": 0.01,
             },
@@ -80,7 +72,7 @@ class StatisticalAnalysis:
 
         self.run_syst = None
 
-        self.template_bin = 5000
+        self.template_bin = 1000
         self.template_bin_edges = np.linspace(0, 1, self.template_bin + 1)
         self.template_sig = {
             'nominal': None,
@@ -155,7 +147,6 @@ class StatisticalAnalysis:
         return templates
 
 
-<<<<<<< HEAD
     def compute_mu(self, observed_data, weight_data):
         self.bins = 101
         # self.bins = 20
@@ -164,49 +155,6 @@ class StatisticalAnalysis:
 
         # TODO: statistic test. Fix this
         data_templates = self.build_data_templates(observed_data, weight_data)
-=======
-    def build_data_tes_templates(self, data, weights=None):
-        # build data template for each score bin. The template is the sow as a function of tes and jes
-
-        tes_range = self.alpha_ranges['tes']['range']
-        histograms = np.zeros((self.bins, len(tes_range)))
-
-        template_x = []
-        for i_tes, tes in enumerate(tes_range):
-            data_reversed = reverse_parameterize_systs(data.copy(), tes)
-            score = self.model.predict(data_reversed)
-            hist, bins = np.histogram(score, bins=self.bin_edges, density=False, weights=weights)
-            histograms[:, i_tes] = hist
-            template_x.append(tes)
-        
-        # build the template for each score bin
-        templates = [None for _ in range(self.bins)]
-
-        def build_template(hist_vs_tesid):
-            coef = np.polyfit(template_x, hist_vs_tesid, 5)
-            def template(tes, jes=1):
-                return np.polyval(coef, tes)
-            return template
-
-        for i in range(self.bins):
-            templates[i] = build_template(histograms[i])
-        
-        return templates
-
-
-    def compute_mu(self, observed_data, weight_data):
-        data_nominal = reverse_parameterize_systs(observed_data.copy())
-        data_score = self.model.predict(data_nominal)
-        obs_count = (data_score > 0.8).sum()
-        self.bins = np.sqrt(obs_count).astype(int)
-        print(f"Number of bins: {self.bins}")
-        self.bin_edges = np.linspace(0.8, 1, self.bins + 1)
-
-        # TODO: statistic test. Fix this
-        # obs_hist, bins = np.histogram(data_score, bins=self.bin_edges, density=False, weights=weight_data)
-        # data_templates = self.build_data_templates(observed_data, weight_data)
-        data_templates = self.build_data_tes_templates(observed_data, weight_data)
->>>>>>> parent of 9cd1d09... first submission
 
         def get_yields(template, alpha):
             yields = self.rebin_hist(template['nominal'], self.template_bin_edges, self.bin_edges)
@@ -263,24 +211,8 @@ class StatisticalAnalysis:
 
         for key, value in self.alpha_ranges.items():
             result.limits[key] = (value['range'][0], value['range'][-1])
-<<<<<<< HEAD
         result.limits['mu'] = (0, 3)
 
-=======
-        result.limits['mu'] = (-10, 20)
-        result.limits['mu'] = (0, 10)
-
-        # TODO: remove the fixed parameters
-        for p in [
-            'jes', 
-            'soft_met', 
-            # 'ttbar_scale', 
-            # 'diboson_scale', 
-            # 'bkg_scale', 
-            # 'tes'
-            ]:
-            result.fixed[p] = True
->>>>>>> parent of 9cd1d09... first submission
 
         result.errordef = Minuit.LIKELIHOOD
         result.migrad()
